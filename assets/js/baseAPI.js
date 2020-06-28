0277dd78d3a2290c2b2aa3007d15366c7a53b128
Melrose <2713605684@qq.com>
2020-06-28 20:27:54 +0800

    完成了后台主页的代码

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,23 @@
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function (value) {
-    console.log(value.url);
     value.url = "http://ajax.frontend.itheima.net" + value.url
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (value.url.indexOf("/my/") != -1) {
+        value.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 complete 回调函数
+    value.complete = function (res) {
+        console.log(res.responseJSON);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            //强制清空token--防止有人输入假token！
+            localStorage.removeItem("token")
+            //不允许页面跳转
+            location.href = "login.html"
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+$(function () {
+    getUserInfo()
+    //写退出功能--主要实现两个功能（清除本地存储的token，和跳转回登录页面）
+    $("#close").click(function (e) {
+        layui.layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //清除本地存储的token
+            localStorage.removeItem("token")
+            //跳转回登录页面
+            location.href = "/login.html"
+            layer.close(index);
+        });
+    });
+})
+
+function getUserInfo() {
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+        success: function (res) {
+            console.log(res);
+            if (res.status != 0) return layui.layer.msg(res.message)
+            //调用renderAvatar 渲染用户的头像
+            renderAvatar(res.data)
+        }
+    });
+}
+
+function renderAvatar(user) {
+    //获取用户名
+    var name = user.nickname || user.username
+    //渲染欢迎的文本
+    $("#welcome").html("欢迎&nbsp;&nbsp;" + name)
+    //判断用户是否有头像，如果有的话就显示头像--根据头像的根路径，如果没有的话则渲染用户名的名字的一个大写
+    if (user.user_pic) {
+        $(".text-avater").hide()
+        $(".layui-nav-img").attr("src", user.user_pic).show()
+
+    } else {
+        $(".layui-nav-img").hide()
+        $(".text-avater").html(name[0].toUpperCase()).show()
+    }
+}
