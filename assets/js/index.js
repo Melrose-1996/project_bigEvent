037d0d47411e258f756ca5373fc9c3e15189dd23
Melrose <2713605684@qq.com>
2020-06-29 16:46:11 +0800

    略做修改

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,5 @@
 $(function () {
     getUserInfo()
-    //写退出功能--主要实现两个功能（清除本地存储的token，和跳转回登录页面）
     $("#close").click(function () {
         layui.layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
             //清除本地存储的token
@@ -29,7 +28,7 @@ function renderAvatar(user) {
     //获取用户名
     var name = user.nickname || user.username
     //渲染欢迎的文本
-    $("#welcome").html("欢迎&nbsp;&nbsp;" + name)
+    $("#welcome").html("欢迎&nbsp;&nbsp;&nbsp;" + name)
     //判断用户是否有头像，如果有的话就显示头像--根据头像的根路径，如果没有的话则渲染用户名的名字的一个大写
     if (user.user_pic !== null) {
         $(".text-avater").hide()
